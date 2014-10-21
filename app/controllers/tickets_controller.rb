b13b9903ae803cc4f5b8bab309e95de4454acec0
Ramon-D <ramon.dina@hotmail.de>
2014-10-21 14:43:34 +0200

    Restricts reading tickets to correct project scope

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,7 +2,7 @@ class TicketsController < ApplicationController
 
 	before_action :set_project
 	before_action :set_ticket, only: [:show, :edit, :update, :destroy]
-  before_action :require_signin!, except: [:show, :index]
+  before_action :require_signin!
 
 	def new
 		@ticket = @project.tickets.build
@@ -40,9 +40,13 @@ class TicketsController < ApplicationController
 	end
 
 	private
-  	def set_project
-    	@project = Project.find(params[:project_id])
-  	end
+    def set_project
+      @project = Project.for(current_user).find(params[:project_id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking " +
+                      "for could not be found."
+      redirect_to root_path
+    end
 
   	def set_ticket
   		@ticket = @project.tickets.find(params[:id])
